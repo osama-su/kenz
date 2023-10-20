27c97eee6dd106dcd11094728fd9ca8092a32757
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-10-20 15:57:08 +0300

    enhancments

--- resources/views/dashboard/returnBills/buttons.blade.php
+++ resources/views/dashboard/returnBills/buttons.blade.php
@@ -7,4 +7,17 @@
             </a>
         @endif
     @endcan
+    @can('delete_delivery')
+        @if($bill->deleted_at==null)
+            <a data-url="{{ route('dashboard.deliveries.destroy',['delivery' => $bill->id]) }}"
+               data-item-id="{{ $bill->id }}"
+               class="btn btn-sm btn-clean btn-icon btn-icon-md delete-button" data-toggle="modal"
+               data-target="#delete_modal">
+                <i class="la la-trash"></i>
+            </a>
+        @endif
+    @endcan
 </td>
+@include('dashboard.includes.delete-modal',['action_message' => 'هذه الفاتورة'])
+
+<script src="{{ asset('assets/js/delete-item.js') }}" type="text/javascript"></script>
