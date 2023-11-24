67ee4458cbca04ee9473113aee823b0367b9770a
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-11-24 16:34:08 +0200

    fix delete modal

--- resources/views/dashboard/returnBills/buttons.blade.php
+++ resources/views/dashboard/returnBills/buttons.blade.php
@@ -18,6 +18,28 @@
         @endif
     @endcan
 </td>
-@include('dashboard.includes.delete-modal',['action_message' => 'هذه الفاتورة'])
+{{--@include('dashboard.includes.delete-modal',['action_message' => 'هذه الفاتورة'])--}}
+<!--begin::Modal-->
+<div class="modal fade" id="delete_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">مسح</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>هل تريد مسح {{ isset($action_message) ? $action_message : 'this item' }}
+                    ؟</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">الغاء</button>
+                <button type="button" class="btn btn-danger" id="delete-button">مسح</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--end::Modal-->
 
 <script src="{{ asset('assets/js/delete-item.js') }}" type="text/javascript"></script>
