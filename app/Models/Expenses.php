429ef7b161e735dd176564cedde1cb7f5ae1ecba
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-12-04 17:31:04 +0200

    expenses model and controller

---
+++ app/Models/Expenses.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expenses extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'amount',
+        'image',
+        'created_by',
+    ];
+
+
+}
