dac763ef6c906a6adb7263f572557c1d2155ddfd
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-12-05 23:31:48 +0200

    sales can only edit his bills

--- resources/views/dashboard/returnBills/buttons.blade.php
+++ resources/views/dashboard/returnBills/buttons.blade.php
@@ -1,20 +1,24 @@
 <td>
     @can('update_delivery')
         @if($bill->deleted_at==null)
+            @if($bill->created_by==auth()->user()->id)
             <a href="{{route('dashboard.deliveries.edit', ['delivery' => $bill->id])}}"
                class="btn btn-sm btn-clean btn-icon btn-icon-md">
                 <i class="la la-edit"></i>
             </a>
+            @endif
         @endif
     @endcan
     @can('delete_delivery')
         @if($bill->deleted_at==null)
-            <a data-url="{{ route('dashboard.deliveries.destroy',['delivery' => $bill->id]) }}"
+                @if($bill->created_by==auth()->user()->id)
+                <a data-url="{{ route('dashboard.deliveries.destroy',['delivery' => $bill->id]) }}"
                data-item-id="{{ $bill->id }}"
                class="btn btn-sm btn-clean btn-icon btn-icon-md delete-button" data-toggle="modal"
                data-target="#delete_modal">
                 <i class="la la-trash"></i>
             </a>
+                @endif
         @endif
     @endcan
 </td>
