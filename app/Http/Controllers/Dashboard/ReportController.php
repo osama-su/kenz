0483a78c4ee2b92d83fb397d850ec32921838895
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-12-28 19:20:32 +0200

    bugs fix

--- app/Http/Controllers/Dashboard/ReportController.php
+++ app/Http/Controllers/Dashboard/ReportController.php
@@ -7,10 +7,9 @@ use App\Models\Bill;
 use App\Models\Company;
 use App\Models\Expenses;
 use App\Models\Product;
-use App\Models\User;
 use App\Models\Supplier;
+use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class ReportController extends Controller
 {
@@ -25,7 +24,7 @@ class ReportController extends Controller
 
         $users = User::where('role_id', '1')->get();
 
-       $suppliers = Supplier::all();
+        $suppliers = Supplier::all();
 
         $products = Product::all();
 
@@ -35,7 +34,7 @@ class ReportController extends Controller
 
         $allUsers = User::where('role_id', '!=', '2')->get();
 
-        return view('dashboard.reports.index', compact('allUsers','users_m', 'suppliers','users', 'products', 'companies'));
+        return view('dashboard.reports.index', compact('allUsers', 'users_m', 'suppliers', 'users', 'products', 'companies'));
     }
 
     public function show(Request $request)
@@ -83,57 +82,78 @@ class ReportController extends Controller
             $bills = $bills->whereHas('billDetails', function ($q) use ($request) {
                 $q->where('product_id', $request->product_id);
 
-                if($request->delivery_status=='no'){
-               $q->withTrashed()->where('delivery_status', 'no');
-                }
-                     if($request->delivery_status=='yes'){
-               $q->withTrashed()->where('delivery_status', 'yes')->where('delivery_status',null);
-                }
+//                if ($request->delivery_status == 'no') {
+//                    $q->withTrashed()->where('delivery_status', 'no');
+//                }
+//                if ($request->delivery_status == 'yes') {
+//                    $q->withTrashed()->where('delivery_status', 'yes')->where('delivery_status', null);
+//                }
             });
         }
-        if ($request->product_id==null) {
+//        if ($request->product_id == null) {
 
         if ($request->delivery_status) {
             $bills = $bills->wherehas('billDetails', function ($q) use($request) {
-                $q->withTrashed()->where('delivery_status', $request->delivery_status);
+                $q->withTrashed()->where('delivery_status', $request->delivery_status = 'pending' ? null : $request->delivery_status);
             });
         }
-}
+//}
         if ($request->delivery_status == 'cancel') {
             $bills = $bills->where('deleted_type', 'cancel');
 
         }
 
-        $bills = $bills->get();
-
+        $bills = $bills
+            ->get();
 
         $profit = 0;
-        $expenses = $expenses->get()->sum('amount');
+        $totalSales = 0;
+        $totalCost = 0;
+        $totalShipping = 0;
+        $totalExpenses = $expenses->whereBetween(
+            'created_at', $bills->count() == 0 ? [null, null] :
+            [$bills->last()->created_at, $bills->first()->created_at])
+            ->get()->sum('amount');
 
-        foreach ($bills as $bill) {
-            $profit += $bill->billDetails->map(static function ($billDetails) {
-                    return $billDetails->product ? $billDetails->product->wholesale_price * $billDetails->qty : 0;
-                })->sum()-$bill->delivery_fee;
+
+        $billsCalc = $bills->where('delivery_status', 'yes');
+        foreach ($billsCalc as $bill) {
+            $totalSales += $bill->price_after;
         }
 
-        $profit = $profit - $expenses;
+        foreach ($bills as $bill) {
+            $totalCost += $bill->billDetails->map(static function ($billDetails) {
+                return $billDetails->product ? $billDetails->product->wholesale_price * $billDetails->qty : 0;
+            })->sum();
 
-        return view('dashboard.reports.show', compact('bills', 'profit'));
+            $totalShipping += $bill->delivery_fee;
+        }
+
+
+        $profit = $totalSales - $totalCost - $totalShipping - $totalExpenses;
+        // check if profit is negative
+        if($profit < 0){
+            $profit = 0;
+        }
+
+//        dd(compact('totalSales','totalCost','totalShipping','totalExpenses','profit'));
+
+        return view('dashboard.reports.show', compact('bills', 'totalSales', 'totalCost', 'totalShipping', 'totalExpenses', 'profit'));
     }
 
-        function generate_pdf(Request $request)
+    function generate_pdf(Request $request)
     {
-        if(!$request->printer){
-                    return redirect()->back()->with(['status' => 'danger', 'message' => 'من فضلك اختار الفاتورة ']);
+        if (!$request->printer) {
+            return redirect()->back()->with(['status' => 'danger', 'message' => 'من فضلك اختار الفاتورة ']);
 
         }
 
         $bills = Bill::withTrashed()->whereIn('id', $request->printer)->get();
 
 
-      $pdf = \PDF::loadView('dashboard.bills.pdf', $bills->first());
+        $pdf = \PDF::loadView('dashboard.bills.pdf', $bills->first());
 
-      return $pdf->stream('bill_' . $bills->first()->id . '.pdf');
+        return $pdf->stream('bill_' . $bills->first()->id . '.pdf');
 
         // return redirect()->back();
     }
